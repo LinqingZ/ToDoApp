d8c72eb600e857a83877f8021c5d4dfb9adea766
LinqingZ <linqing201@gmail.com>
2023-11-09 21:27:04 -0500

    Initial commit with Todo App files

---
+++ api.py
@@ -0,0 +1,65 @@
+from flask import Flask, jsonify, request, json
+from flask_sqlalchemy import SQLAlchemy
+from langchain.utilities import SQLDatabase
+# from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+from langchain_experimental.sql import SQLDatabaseChain
+
+OPENAI_API_KEY="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"
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example.db"
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+
+    def __str__(self):
+        return f'{self.id}, {self.content}'
+    
+
+def todo_serializer(todo):
+    return {
+        "id": todo.id,
+        'content': todo.content
+    }
+
+@app.route('/api', methods = ['GET'])  # don't need to put /api on it
+def index(): 
+    # todo = Todo.query.all() # can led to error if type Todo is not JSON serializiable
+    return jsonify([*map(todo_serializer, Todo.query.all())]) # unpack object into array
+
+@app.route('/api/create', methods=['POST'])
+def create():
+    request_data = json.loads(request.data)
+    todo = Todo(content=request_data['content'])
+
+    db.session.add(todo)
+    db.session.commit()
+    return {'201': 'todo created successfully'}
+
+@app.route('/api/<int:id>')
+def show(id):
+    return jsonify([*map(todo_serializer, Todo.query.filter_by(id=id))])
+
+
+
+@app.route('/api/ask', methods=['POST'])
+def ask():
+    print("asked")
+    db_uri = "sqlite:///C:/Users/xiuwen/Linqing/CTP/Todo App/instance/example.db"
+    db = SQLDatabase.from_uri(db_uri)
+    llm = ChatOpenAI(temperature=0, verbose=True, openai_api_key=OPENAI_API_KEY)
+    db_chain = SQLDatabaseChain.from_llm(llm, db, verbose=True)
+    question = request.data.decode('utf-8')
+    answer = db_chain.run(question)
+    print(question)
+    print(type(answer))
+    return jsonify({'answer': answer})
+    # return {'202': 'Ai answered question successfully'}
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
